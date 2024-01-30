2bf29851c8187dee1900a38d4f8e68ee0156e88f
Pri-Destro <70753520+Pri-Destro@users.noreply.github.com>
2024-01-30 17:32:33 +0530

    quad_roots.cpp

--- linked list/maths/quad_roots.cpp
+++ linked list/maths/quad_roots.cpp
@@ -4,21 +4,19 @@ using namespace std;
 
 
 long long factorial(int N){
-    if (N == 0) {return 1;}
+    if (N == 0) return 1;
     return N * factorial(N-1);
 }
 
 
-int digitsInFactorial(int N)
-    {
-        // code here
-        int count = log(factorial(N)) + 1;
+int digitsInFactorial(int N)  // Finding how many digits are in a factorial of a number
+    {            
+        int count = log(factorial(N)) + 1;        // log of a number == number of digits - 1
         return count;
     }
 int main()
 {
 
     cout << digitsInFactorial(42) << endl;
-    // cout << factorial(42) << endl;
     return 0;
 }
